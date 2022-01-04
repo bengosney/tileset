476ef08233bdacd8c8768440f3b9001116c2165d
Ben Gosney <bengosney@googlemail.com>
2022-01-04 14:04:01 +0000

    Ignore pngs

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ dark: RGB = (60, 65, 71)
 darker: RGB = (27, 35, 43)
 
 size = 32
-for i in range(1):
+for i in range(10):
     tile = Tile((size, size), light, dark, darker, noise_scale=1, seed=i)
     tile.add_top(size // 4)
     tile.save(20)
